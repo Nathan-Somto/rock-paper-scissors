4e3c58fc10b60608472c6df52acf4c41163de68c
Mkparu Somtochi <mkparusomtochi26@gmail.com>
2022-08-26 22:50:47 +0100

    finished majority of the js but the button bug still remains :(

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-// user input
+// dom values
+
+const reset =document.querySelector("#reset");
+const overlay =document.querySelector(".overlay");
+const modal =document.querySelector(".modal");
+const modalTitle=document.querySelector("#modalTitle");
+const btn=document.querySelector('.buttons');
+const userScore= document.querySelector('#userScore');
+const computerScore =document.querySelector("#computerScore");
+const uiResult =document.querySelector('.choice');
+const computerSelect =document.querySelector("#computerSelect");
+const userSelect =document .querySelector("#userSelect");
+const imagePath=["/Javascript/projects/rock-paper-scissors/rock.svg","/Javascript/projects/rock-paper-scissors/paper.svg","/Javascript/projects/rock-paper-scissors/scissors.svg"];
+let computerChoices="";
+let winner="";
+// initalized values
+let round=0;
+let result="";
+let userCount=0;
+let computerCount=0;
 const numberOfRounds=5;
-//const playerName=prompt("Enter player name : ");
-const uiResult=document.querySelector('.choice');
 // the getComputerChoice function
 function random (number){
     return Math.floor(Math.random()*number);
@@ -13,18 +30,22 @@ function getComputerChoice(){
 };
 function playRound(playerSelection,computerSelection=getComputerChoice()){
     let correctPlayerSelection=playerSelection.toLowerCase();
+    computerChoices=computerSelection;
     // check value being passed to the function
     if(correctPlayerSelection==="paper"|| correctPlayerSelection==="scissors"||correctPlayerSelection==="rock"){
     
         //computer wins
     if(correctPlayerSelection==="rock" && computerSelection==="paper"){
-        return "You lose! Paper beats Rock !";
+        winner="computer";
+        return "Paper beats Rock !";
     }
     else if(correctPlayerSelection==="paper"&&computerSelection==="scissors"){
-        return "You lose! scissors cuts paper";
+         winner= "computer";
+        return "Scissors cuts paper";
     }
     else if(correctPlayerSelection==="scissors" &&computerSelection==="rock"){
-        return "You lose! rock smashes scissors"
+        winner="computer";
+        return "Rock smashes scissors"
     }
 
     //ties
@@ -35,68 +56,137 @@ function playRound(playerSelection,computerSelection=getComputerChoice()){
 
     // player wins
     else if(correctPlayerSelection==="paper" && computerSelection==="rock"){
-        return "You win! Paper beats Rock !";
+        winner ='player';
+        return "Paper beats Rock !";
     }
     else if(correctPlayerSelection==="scissors"&&computerSelection==="paperr"){
-        return "You win! scissors cuts paper";
+        winner='player';
+        return " Scissors cuts paper";
     }
     else if(correctPlayerSelection==="rock" && computerSelection==="scissors"){
-        return "You win! rock smashes scissors"
+        winner='player';
+        return "Rock smashes scissors"
     }}
    
     else{
         return "Check your Input";
     }
 }
+// displayModal function
+function displayModal(){
+    if(userCount>computerCount){
+        modalTitle.textContent='you won! :)';
+    }
+    else if (computerCount>userCount){
+        modalTitle.textContent='you lost! :(';
+    }
+   
+    overlay.classList.remove('inactive');
+    modal.classList.remove('inactive');
+    overlay.classList.add('active');
+    modal.classList.add('active');
+}
 // call game function
 // image change for when button is clicked
-const btn=document.querySelector('.buttons');
-let round=0;
-let result="";
-let userCount=0;
-let computerCount=0;
+function setImageUser(event){
+    // accepts events inner child as an argument and set the source 
+   /*  event.target. */
+   if(event.target.id==="rock"){
+    userSelect.setAttribute('src',imagePath[0]);
+   }
+   else if(event.target.id==='scissors'){
+    userSelect.setAttribute('src',imagePath[2]);
+   }
+   else if(event.target.id=="paper"){
+    userSelect.setAttribute('src',imagePath[1]);
+}
+
+
+   
+}
+  function setImageComputer(){
+     if(computerChoices==='rock'){
+    computerSelect.setAttribute('src',imagePath[0]);
+}
+else if(computerChoices==='paper'){
+    computerSelect.setAttribute('src',imagePath[1]);
+   
+}
+else if(computerChoices==='scissors'){
+
+    computerSelect.setAttribute('src',imagePath[2]);
+} 
+  }
+   
+  function setUiResult(){
+    uiResult.textContent=result;
+  }
+
+
 function game(event){
+    console.log(event);
      if(round!==numberOfRounds){
     if(event.target.id==="rock"){
         result=playRound('rock');
+        setImageUser(event);
+      
+        setTimeout(setImageComputer,1000);
+       
+        setTimeout(setUiResult,2000);
     
     }
     else if(event.target.id==='paper'){
         result=playRound('paper');
+        setImageUser(event);
+      
+        setTimeout(setImageComputer,1000);
+       
+        setTimeout(setUiResult,2000);
     }
     else if(event.target.id==='scissors'){
         result=playRound('scissors');
+        setImageUser(event);
         
+        setTimeout(setImageComputer,1000);
+       
+        setTimeout(setUiResult,2000);
     }
     console.log(result);
     round+=1;
-      if(result.includes("You win!")){
+      if(winner==="player"){
         userCount+=1;
+        userScore.textContent=userCount;
         }
-        else if(result.includes("You lose!")){
-            computerCount+=1
+        else if(winner==='computer'){
+            computerCount+=1;
+            computerScore.textContent=computerCount;
         }
     
     if(round===numberOfRounds){
-        if(userCount>computerCount){
-            uiResult.textContent='you won! :)';
-        }
-        else if (computerCount>userCount){
-            uiResult.textContent='you lost! :(';
-        }
-        else{
-            uiResult.textContent='it ended in a tie! :|';
-        }
+ setTimeout(displayModal(),5000);
+        
     }
     
 }
-
-    
-  
- 
 }
-// create reset game function
+//  to run game function
     btn.addEventListener('click',game);
 
-// modal handler
-const reset =document.querySelector("#");
+// resetGame function
+function resetGame (){
+    round=0;
+    uiResult.textContent="Choose One!";
+    computerScore.textContent=0;
+    userScore.textContent=0;
+    computerCount=0;
+    userCount=0;
+    computerSelect.setAttribute('src','#');
+    userSelect.setAttribute('src','#');
+    overlay.classList.remove('active');
+    modal.classList.remove('active');
+    overlay.classList.add('inactive');
+    modal.classList.add('inactive');
+}
+// image setter funtion
+
+reset.addEventListener('click',resetGame);
